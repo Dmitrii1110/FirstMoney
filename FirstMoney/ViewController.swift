6256373e1c46486fdf32059140b77fb7f554da4b
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-02-04 15:23:00 +0300

    Доделывал вид экрана (кнопки+нижняя панель)

--- FirstMoney/ViewController.swift
+++ FirstMoney/ViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var numberFromKeyboard: [UIButton]!{
+        didSet {
+            for button in numberFromKeyboard {
+                button.layer.cornerRadius = 11
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
