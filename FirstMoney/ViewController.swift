d32e3993eddd547e4466397dca1ffc2259d83de0
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2022-02-05 16:44:14 +0300

    Настроил возможность удаления записей в таблице

--- FirstMoney/ViewController.swift
+++ FirstMoney/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import CoreData
 
 class ViewController: UIViewController {
     
@@ -73,6 +74,7 @@ class ViewController: UIViewController {
         try! realm.write {
             realm.add(value)
         }
+        tableView.reloadData() //обновление экрана после нажатия на кнопки
     }
     
 }
@@ -86,7 +88,35 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         cell.recordCategory.text = spending.category
         cell.recordCost.text = "\(spending.cost)"
         
+        switch spending.category {
+        case "Еда": cell.recordImage.image = #imageLiteral(resourceName: "eda")
+        case "Одежда": cell.recordImage.image = #imageLiteral(resourceName: "odejda")
+        case "Связь": cell.recordImage.image = #imageLiteral(resourceName: "svyz")
+        case "Досуг": cell.recordImage.image = #imageLiteral(resourceName: "dosug")
+        case "Красота": cell.recordImage.image = #imageLiteral(resourceName: "krasota")
+        case "Авто": cell.recordImage.image = #imageLiteral(resourceName: "avto")
+        default: cell.recordImage.image = #imageLiteral(resourceName: "Ultramarine.png")
+        }
+        
         return cell
+    }
+    
+    //ниже функция для удаления записей в tableView
+    //Нужно найти решение как работает новый метод!!!!
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let editingRow = spendingArray[indexPath.row]
+        let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") {  (contextualAction, view, boolValue) in
+            
+            try! self.realm.write {
+                self.spendingArray.realm?.delete(editingRow)
+                            tableView.deleteRows(at: [indexPath], with: .automatic)
+                        }
+            
+        }
+        let swipeActions = UISwipeActionsConfiguration(actions: [deleteAction])
+
+        return swipeActions
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
